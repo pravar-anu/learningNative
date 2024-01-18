cc37267c91103a0a42adcd8de04db23baea2f94e
Pravar Anu <pravar.anu@arcadis.com>
2024-01-18 13:06:34 +0530

    Personalized page depending on route.params

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import StylingExercise from './src/components/StylingExrecise';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ColorPalette from './src/screens/ColorPalette';
+import Home from './src/screens/Home';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <>
-      <StylingExercise />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="ColorPalette"
+          component={ColorPalette}
+          options={({ route }) => ({ title: route.params?.paletteName })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
